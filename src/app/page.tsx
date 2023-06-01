4fc626a67a5b0c7d218a8ffad676ddf724580bb9
Andrei Calazans <andrei.calazans@coinbase.com>
2023-05-31 22:10:49 -0300

    add link

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,11 @@
-import Image from 'next/image'
 import { H1 } from "./components/h1";
+import { A } from './components/a';
 
 export default function Home() {
   return (
   <main>
     <H1>React Native Weekly</H1>
+    <A href="/changelog/week-22-2023">Week 22 - 2023</A>
   </main>
   )
 }
